13da102b77baced7fde137f349f7952f4e29e5d4
Dan Sabadis <dsabadis@sdl.com>
2019-03-02 23:25:14 +0200

    done basic unit testing with mocking Console via NSubstitute

--- RobotCleaningLogicUnitTests/ConsoleParserUnitTests.cs
+++ RobotCleaningLogicUnitTests/ConsoleParserUnitTests.cs
@@ -1,9 +1,25 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using RobotCleaningLogic;
 
 namespace RobotCleaningLogicUnitTests
 {
     [TestClass]
     public class ConsoleParserUnitTests
     {
+        [TestMethod]
+        public void Invalid_HorizontalCoordinates_Fail()
+        {
+            IStandardIOReaderWriter consoleMock = Substitute.For<IStandardIOReaderWriter>();
+            consoleMock
+                .ReadLine().Returns("4", "10 20", "E 2", "E 5", "W 3", "S 7");
+
+            var sut = new ConsoleParser(consoleMock);
+            var inputCommand = sut.ParseAllSteps();
+
+            Assert.IsTrue(inputCommand.StartingCoordinate.X == 10);
+            Assert.IsTrue(inputCommand.StartingCoordinate.Y == 20);
+            Assert.IsTrue(inputCommand.AtomicNavigationSteps.Count == 17);
+        }
     }
 }
